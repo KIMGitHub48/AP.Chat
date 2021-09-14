fc64ab703ce22d55becde1fdcb8552cac2f6d7e7
veiled86 <67363081+veiled86@users.noreply.github.com>
2021-09-14 17:00:36 +0700

    Добавляем Dao для подключения SQL базы и работы с ней. Добавляем Entity классы SQL базы

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires reflections;
+    requires java.persistence;
+    requires java.sql;
 
     opens org.presentation to javafx.fxml;
     exports org.presentation;
