17209ffb227ddd93993d4506c8150cdbc5991ba8
veiled86 <67363081+veiled86@users.noreply.github.com>
2021-09-23 11:10:28 +0700

    Классы в папках

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module org {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires reflections;
+    requires java.persistence;
+    requires java.sql;
 
     opens org.Presentation to javafx.fxml;
     exports org.Presentation;
--- src/main/java/org/DATA/Facade.java
+++
@@ -1,4 +0,0 @@
-package org.DATA;
-
-public class Facade {
-}
